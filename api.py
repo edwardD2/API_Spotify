ae0f2ff9e5a3cc3405dd8288f144ca6011fa290c
Edward Dela Cruz <edwarddc7122@gmail.com>
2022-02-06 18:28:54 -0800

    updated files

--- api.py
+++ api.py
@@ -17,7 +17,7 @@ auth_response = requests.post(AUTH_URL, {
     'client_secret': CLIENT_SECRET,
 })
 
-# convert the response to JSON
+# convert the response to json
 auth_response_data = auth_response.json()
 
 # save the access token
